c5dddb576a08a304d33f3af7ce567f8d52a28e83
Amiiiiiine <ka_adjou@esi.dz>
2023-08-13 02:47:04 -0700

    Update it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	fwrite(a, 59, 1, stderr);
 	return (1);
 }
