5a6718fec391139d62edfdb031f2b57f6c4511fd
Amiiiiiine <ka_adjou@esi.dz>
2023-08-13 01:45:51 -0700

    Update Betty return warning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - printing
- * Description : this is just a simple function!
+ * Description: this is just a simple function!
  *
- * Return : it returns always 0
+ * Return: always 0
  */
 int main(void)
 {
