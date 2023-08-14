b148739bb9874a4890bf0380c464157d1f7d8a57
Amiiiiiine <ka_adjou@esi.dz>
2023-08-14 12:12:11 -0700

    Add print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - a simple function
+ * Description: just a simple function that prints letters with putchar.
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+
+	int i, j, z;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (z = j + 1; z <='9'; z++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(z);
+				if (i != '7' || j != '8' || z != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
