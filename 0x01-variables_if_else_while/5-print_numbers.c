50f3d30743d27fd8dee04791486a6a13dfb43122
Amiiiiiine <ka_adjou@esi.dz>
2023-08-14 00:51:40 -0700

    Add numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - a simple function
+ * Description: just a simple function that prints numbers with putchar.
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
