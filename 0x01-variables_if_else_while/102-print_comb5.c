790dadd09143bd1c9a33625554d0f680ea2d9c47
Amiiiiiine <ka_adjou@esi.dz>
2023-08-14 12:54:48 -0700

    Add best combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main - a simple function
+ * Description: just a simple function that prints letters with putchar.
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+
+	int i, j, z, w;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				for (w = '0'; w <= '9'; w++)
+				{
+					if (i == z && w > j || i < z)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(z);
+						putchar(w);
+					if (i != '9' || j != '8' || z != '9' || w != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
