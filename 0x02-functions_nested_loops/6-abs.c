5d2662f552cf5dc38f574082a66b898c1e4874ea
Amiiiiiine <ka_adjou@esi.dz>
2023-08-19 15:59:09 -0700

    Add abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+
+/**
+ * _abs - simple one
+ * @n: just an int
+ *
+ * Return: the abs value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	else 
+	{
+		return (n);
+	}
+}
