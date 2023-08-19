4ffa0a3b99540c8e16e71f3d8bfe3165346b66cb
Amiiiiiine <ka_adjou@esi.dz>
2023-08-19 06:36:09 -0700

    Add printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - a simple one
+ * Description: just a simple function that returns an int.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
