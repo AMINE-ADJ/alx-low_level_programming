6cf6760a4d50dc4987dded7b9f65a5abac260bda
Amiiiiiine <ka_adjou@esi.dz>
2023-08-22 16:55:21 -0700

    Update array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,8 @@ void print_array(int *a, int n)
 
 	int i;
 
+	if (n > 0)
+	{
 	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
@@ -22,5 +24,5 @@ void print_array(int *a, int n)
 			printf("%d\n", a[i]);
 		}
 	}
-
+	}
 }
