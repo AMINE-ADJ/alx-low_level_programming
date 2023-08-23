8ae77e8465cfec65a852a14d64c8ac0494fcaf1f
Amiiiiiine <ka_adjou@esi.dz>
2023-08-23 05:31:21 -0700

    Add atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+
+/**
+ * _atoi - converts a string to int
+ * @s: string to convert
+ *
+ * Return: the signed int.
+ */
+int _atoi(char *s)
+{
+	int n, i, sign;
+
+	n = 0;
+	i = 0;
+	sign = 1;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = (n * 10) + (s[i] - '0');
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (n > 0)
+		{
+			break;
+		}
+
+		i++;
+	}
+	return (sign * n);
+}
