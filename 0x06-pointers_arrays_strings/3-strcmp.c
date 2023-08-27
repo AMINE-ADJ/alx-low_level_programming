cd50d229f0b36253503b308e3bd64bd702f2969a
Amiiiiiine <ka_adjou@esi.dz>
2023-08-27 10:13:48 -0700

    Update strcmp 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,26 +9,30 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
+	int cmp = 0;
 
-	if (s1[i] == '\0')
-		return (-1);
-	if (s2[i] == '\0')
-		return (1);
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
+		/*
+		 * if (s1[i] == s2[i])
+		 * {
+		 *	continue;
+		 * }
+		 * else if (s1[i] < s2[i])
+		 * {
+		 *	return (-1);
+		 * }
+		 * else
+		 * {
+		 *	return (1);
+		 *}
+		 */
+		if (s1[i] != s2[i])
 		{
-			continue;
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else
-		{
-			return (1);
+			cmp = ((int) s1[i] - 48) - ((int)s2[i] - 48);
+			break;
 		}
 	}
-	return (0);
+	return (cmp);
 }
