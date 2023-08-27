dad437e33d5191164f923419b64bd112a7cd5b2d
Amiiiiiine <ka_adjou@esi.dz>
2023-08-27 10:40:21 -0700

    Add rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+
+/**
+ * reverse_array - reversing array
+ * @a: array
+ * @n: length of a
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
+}
