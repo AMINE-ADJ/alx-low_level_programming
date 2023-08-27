ccefb9f9dd0b3c29ff6cdf90a25a347222ecca46
Amiiiiiine <ka_adjou@esi.dz>
2023-08-27 09:56:46 -0700

    Update strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,6 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
+	if (s1[i] == '\0')
+		return (-1);
+	if (s2[i] == '\0')
+		return (1);
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
