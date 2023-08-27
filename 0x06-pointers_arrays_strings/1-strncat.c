0faf9ef93a52cb30271a8e123971dab72fc88447
Amiiiiiine <ka_adjou@esi.dz>
2023-08-27 07:21:52 -0700

    Update strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int enddst = strlen(dest);
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[enddst + i] = src[i];
 	}
